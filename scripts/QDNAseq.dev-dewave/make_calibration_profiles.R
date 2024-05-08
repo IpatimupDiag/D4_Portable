9368d1ea66508b0f46f9912c7be2d7fb45cd727d
jpm <jpm@mail.com>
2024-05-08 07:39:13 -0400

    jpm: Added new normal for the calibration of reads from ioturrent. Also, a new script is avalible to turn the avaliable normals into calibration profiles.

---
+++ scripts/QDNAseq.dev-dewave/make_calibration_profiles.R
@@ -0,0 +1,72 @@
+#
+
+# Create new calibration profiles for deWave from normals.
+# All variables with '!!!'  as coment, chanhe the bins size value.
+
+#
+library(gtools)
+library(impute)
+library(MASS)
+library(limma)
+library(QDNAseq)
+library(Biobase)
+
+sourceDir <- function(path, trace = TRUE, ...) {
+    for (nm in list.files(path, pattern = "\\.[RrSsQq]$")) {
+       if(trace) cat(nm,":")           
+       source(file.path(path, nm), ...)
+       if(trace) cat("\n")
+    }
+}
+sourceDir("R/") # get deWave local lib
+
+
+#Normals input directory
+in_dir <- "/home/jpparrachadematos/new_D4_dir/normalsData_deWave/IPATIMUP_normals/IPATIMUP_normals_pack1/"
+
+#Bin size !!!
+# Change for the disired bin size.
+bin_size <- 1000
+
+#Calibration profiles directory
+out_dir <- "data_ionTurrent/"
+
+
+#start
+
+bins <- getBinAnnotations(binSize=bin_size)
+
+print("Get the normals:")
+
+readCounts <- binReadCounts(bins, path=in_dir)
+
+print("Filtering the normals:")
+
+readCountsFiltered <- applyFilters(readCounts)
+readCountsFiltered <- estimateCorrection(readCountsFiltered)
+copyNumbers <- correctBins(readCountsFiltered)
+
+print(copyNumbers)
+
+print("Making the calibration profiles:")
+
+# NormalCalibrationSet_100kb !!!
+# Change the bins size in the name of the variable, this is due to the name of the dataset beeing the same as the variable.
+NormalCalibrationSet_1000kb <- createNormalCalibration(copyNumbers)
+
+
+#save calibration profiles
+
+calibration_set_name <- paste("NormalCalibrationSet_", bin_size, "kb.rda",sep="")
+
+calibration_dir <- paste(out_dir, calibration_set_name,sep="")
+
+# NormalCalibrationSet_100kb !!!
+# Also change were.
+save(NormalCalibrationSet_1000kb,file=calibration_dir)
+
+#saveRDS(NormalCalibrationSet,calibration_dir)
+
+
+
+print(paste("Done!", calibration_set_name))
