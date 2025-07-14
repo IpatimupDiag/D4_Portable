a6d7d44c071add5105afa317b748e07b4f2af3ae
JPMatos <44948470+jppmatos@users.noreply.github.com>
2025-07-14 12:10:18 +0100

    Update clonality.R
    Signed-off-by: JPMatos <44948470+jppmatos@users.noreply.github.com>

--- scripts/d4-pipeline-master/bin/clonality.R
+++ scripts/d4-pipeline-master/bin/clonality.R
@@ -92,9 +92,10 @@ if((clonTab$cor > 0.54 & clonTab$llr2 > 0) & Non_clonal_prob > 0.5){
 }else if((clonTab$llr2 < -5 | clonTab$cor < 0.45) & Non_clonal_prob < 0.5){
     outlier <- T
     main <- 'This sample pair is an outlier and possibly clonal'
-}else if((clonTab$llr2 > -5 & clonTab$llr2 < 0) & (clonTab$cor > 0.45 & clonTab$cor < 0.54)){
-    outlier <- T
-    main <- 'This sample pair is possibly inconclusive'
+#}else if((clonTab$llr2 > -5 & clonTab$llr2 < 0) & (clonTab$cor > 0.45 & clonTab$cor < 0.54)){
+}else if((clonTab$llr2 >= -5 & (clonTab$cor >= 0.45 & clonTab$cor <= 0.54)) | (clonTab$cor >= 0.45 & (clonTab$llr2 >= -5 & clonTab$llr2 <= 0))){
+    outlier <- F
+    main <- ''
 }else{
     outlier <- F
     main = ''
